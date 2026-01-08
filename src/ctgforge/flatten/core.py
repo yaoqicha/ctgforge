0606fec5e87709a3b1db1123c72c4bf0a48d9c1d
littledumb <jiamin@gmail.com>
2026-01-08 17:13:48 +0800

    model extended; need tests

--- src/ctgforge/flatten/core.py
+++ src/ctgforge/flatten/core.py
@@ -1,9 +1,10 @@
-from ..models.core import TrialCore
+from ..models.core import *
 from ..models.provenance import ProvenancedValue
 
 
 def flatten_core(raw: dict) -> TrialCore:
     p = raw.get("protocolSection", {})
+    d = raw.get("derivedSection", {})
 
     ident = p.get("identificationModule", {})
     status = p.get("statusModule", {})
@@ -11,24 +12,60 @@ def flatten_core(raw: dict) -> TrialCore:
     conds = p.get("conditionsModule", {})
     arms = p.get("armsInterventionsModule", {})
     sponsor = p.get("sponsorCollaboratorsModule", {})
+    
+    cond_browse = d.get("conditionBrowseModule", {})
+    intr_browse = d.get("interventionBrowseModule", {})
 
     trial = TrialCore(
         nct_id=ident.get("nctId"),
         brief_title=ident.get("briefTitle"),
         official_title=ident.get("officialTitle"),
-        overall_status=status.get("overallStatus"),
         study_type=design.get("studyType"),
-        phase=design.get("phases", []),
-        start_date=status.get("startDateStruct", {}).get("date"),
-        completion_date=status.get("completionDateStruct", {}).get("date"),
-        sponsor=sponsor.get("leadSponsor", {}).get("name"),
-        conditions=conds.get("conditions", []),
+        overall_status=status.get("overallStatus"),
+        phases=design.get("phases", []),
+        start_date=DateStruct(
+            date=status.get("startDateStruct", {}).get("date"),
+            type=status.get("startDateStruct", {}).get("type"),
+        ),
+        completion_date=DateStruct(
+            date=status.get("completionDateStruct", {}).get("date"),
+            type=status.get("completionDateStruct", {}).get("type"),
+        ),
+        primary_completion_date=DateStruct(
+            date=status.get("primaryCompletionDateStruct", {}).get("date"),
+            type=status.get("primaryCompletionDateStruct", {}).get("type"),
+        ),
+        lead_sponsor=Agency(
+            name=sponsor.get("leadSponsor", {}).get("name"),
+            type=sponsor.get("leadSponsor", {}).get("class"),
+        ),
+        collaborators=[
+            Agency(
+                name=collab.get("name"),
+                type=collab.get("class"),
+            )
+            for collab in sponsor.get("collaborators", [])
+        ],
+        conditions=[
+            Condition(
+                name=c,
+                mesh_uid=next(iter([mesh for mesh in cond_browse.get("meshes", []) if mesh.get("term") == c]), {}).get("id"),
+            )
+            for c in conds.get("conditions", [])
+        ],
         interventions=[
-            {
-                "type": i.get("type"),
-                "name": i.get("name"),
-            }
-            for i in arms.get("interventions", [])
+            Intervention(
+                name=intr.get("name"),
+                type=intr.get("type"),
+                description=intr.get("description"),
+                other_names=intr.get("otherNames", []),
+                arm_group_types=[
+                    next(iter([ag for ag in arms.get("armGroups", []) if ag.get("label") == ag_label]), {}).get("type", "OTHER")
+                    for ag_label in intr.get("armGroupLabels", [])
+                ],
+                mesh_uid=next(iter([mesh for mesh in intr_browse.get("meshes", []) if mesh.get("term") == intr.get("name")]), {}).get("id"),
+            )
+            for intr in arms.get("interventions", [])
         ],
     )
 
--- src/ctgforge/models/core.py
+++ src/ctgforge/models/core.py
@@ -1,25 +1,114 @@
-from typing import Any, Union
+# For protocol definitions, please refer to: https://clinicaltrials.gov/policy/protocol-definitions
+
+from typing import Any, Literal, Optional
 
 from pydantic import BaseModel, Field
 
 from .provenance import ProvenancedValue
 
 
+class Agency(BaseModel):
+    name: str
+    type: Literal[
+        "NIH",
+        "FED",
+        "OTHER_GOV",
+        "INDIV",
+        "INDUSTRY",
+        "NETWORK",
+        "AMBIG",
+        "OTHER",
+        "UNKNOWN"
+    ] # https://clinicaltrials.gov/data-api/about-api/study-data-structure#enum-AgencyClass
+    
+
+class Condition(BaseModel):
+    name: str
+    mesh_uid: Optional[str]
+    
+    
+class DateStruct(BaseModel):
+    date: str
+    type: Literal[
+        "ACTUAL",
+        "ESTIMATED"
+    ] # https://clinicaltrials.gov/data-api/about-api/study-data-structure#enum-DateType
+
+
+class Intervention(BaseModel):
+    name: str
+    mesh_uid: Optional[str]
+    type: Literal[
+        "BEHAVIORAL",
+        "BIOLOGICAL",
+        "COMBINATION_PRODUCT",
+        "DEVICE",
+        "DIAGNOSTIC_TEST",
+        "DIETARY_SUPPLEMENT",
+        "DRUG",
+        "GENETIC",
+        "PROCEDURE",
+        "RADIATION",
+        "OTHER"
+    ] # https://clinicaltrials.gov/data-api/about-api/study-data-structure#enum-InterventionType
+    arm_group_types: Optional[list[Literal[
+        "EXPERIMENTAL",
+        "ACTIVE_COMPARATOR",
+        "PLACEBO_COMPARATOR",
+        "SHAM_COMPARATOR",
+        "NO_INTERVENTION",
+        "OTHER"
+    ]]] # https://clinicaltrials.gov/data-api/about-api/study-data-structure#enum-ArmGroupType
+    other_names: list[str] = Field(default_factory=list)
+    description: Optional[str]
+    
+
 class TrialCore(BaseModel):
     nct_id: str
 
-    brief_title: Union[str, None] = None
-    official_title: Union[str, None] = None
+    brief_title: str
+    official_title: str
+    
+    study_type: Literal[
+        "EXPANDED_ACCESS",
+        "INTERVENTIONAL",
+        "OBSERVATIONAL"
+    ] # https://clinicaltrials.gov/data-api/about-api/study-data-structure#enum-StudyType
 
-    overall_status: Union[str, None] = None
-    phase: list[str] = Field(default_factory=list)
-    study_type: Union[str, None] = None
+    overall_status: Literal[
+        "ACTIVE_NOT_RECRUITING",
+        "COMPLETED",
+        "ENROLLING_BY_INVITATION",
+        "NOT_YET_RECRUITING",
+        "RECRUITING",
+        "SUSPENDED",
+        "TERMINATED",
+        "WITHDRAWN",
+        "AVAILABLE",
+        "NO_LONGER_AVAILABLE",
+        "TEMPORARILY_NOT_AVAILABLE",
+        "APPROVED_FOR_MARKETING",
+        "WITHHELD",
+        "UNKNOWN"
+    ] # https://clinicaltrials.gov/data-api/about-api/study-data-structure#enum-Status
+    
+    phases: list[Literal[
+        "NA",
+        "EARLY_PHASE1",
+        "PHASE1",
+        "PHASE2",
+        "PHASE3",
+        "PHASE4"
+    ]] = Field(default_factory=list) # https://clinicaltrials.gov/data-api/about-api/study-data-structure#enum-Phase
+    
+    lead_sponsor: Agency
+    collaborators: list[Agency] = Field(default_factory=list)
+    
+    conditions: list[Condition] = Field(default_factory=list)
+    interventions: list[Intervention] = Field(default_factory=list)
 
-    start_date: Union[str, None] = None
-    completion_date: Union[str, None] = None
-
-    sponsor: Union[str, None] = None
-    conditions: list[str] = Field(default_factory=list)
-    interventions: list[dict[str, Any]] = Field(default_factory=list)
+    start_date: DateStruct
+    primary_completion_date: DateStruct
+    completion_date: DateStruct
 
     prov: dict[str, ProvenancedValue] = Field(default_factory=dict)
